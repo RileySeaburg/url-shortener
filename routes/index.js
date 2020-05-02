f3f2379d890dc7785465ef2bb21bb9ed719f8b59
Riley Seaburg <riley@rds.marketing>
2020-05-01 21:34:05 -0400

    final commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+/** @format */
+
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+// @route GET /:code
+// @desc Redirect to long/original URL
+router.get('/:code', async (req, res) => {
+	try {
+		const url = await Url.findOne({ urlCode: req.params.code });
+
+		if (url) {
+			return res.redirect(url.longUrl);
+		} else {
+			return res.status(404).json('No Url found');
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json('Server Error');
+	}
+});
+
+module.exports = router;
